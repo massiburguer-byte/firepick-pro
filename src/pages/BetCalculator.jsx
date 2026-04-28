c7faadbfd923bc35b4db6912d8af74a7ee316d66
argenispr <inversionessyaar@gmail.com>
2026-04-28 07:54:30 -0400

    Fixed crash in BetCalculator by adding missing Navbar import

--- src/pages/BetCalculator.jsx
+++ src/pages/BetCalculator.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { useTheme } from '../context/ThemeContext';
+import Navbar from '../components/Navbar';
 import ExpertGuideModal from '../components/ExpertGuideModal';
 import { 
   Calculator, 
